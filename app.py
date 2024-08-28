78a230fc92214c9842f2932ec8a61cec89b421a7
ueno-cmd <ut1222ci7@hotmail.co.jp>
2024-08-28 11:58:05 +0900

    2024/8/28

---
+++ app.py
@@ -0,0 +1,24 @@
+from pycaret.regression import *
+import pandas as pd
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+	payload = request.json
+	columns = ['symboling', 'normalized-losses', 'make', 'fuel-type', 'aspiration', 'num-of-doors',
+			   'body-style', 'drive-wheels', 'engine-location', 'wheel-base', 'length', 'width',
+			   'height', 'curb-weight', 'engine-type', 'num-of-cylinders', 'engine-size',
+			   'fuel-system', 'bore', 'stroke', 'compression-ratio', 'horsepower', 'peak-rpm',
+			   'city-mpg', 'highway-mpg']
+	df = pd.DataFrame(payload['data'], columns=columns)
+	model = load_model('automobile_final')
+	predictions = predict_model(model, df)
+	return jsonify({
+		'price': float(predictions[['prediction_label']].values[0][0])
+	})
+
+
+app.run()
